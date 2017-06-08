112b780a1e6240ea09d3193b6698146b33fefe7b
giovani.b.ximenes@gmail.com <giovani.b.ximenes@gmail.com>
2017-06-08 20:19:20 -0300

    Commit

---
+++ src/main/java/br/com/drg/servicos/implementacoes/UsuarioServiceImplementacao.java
@@ -0,0 +1,7 @@
+package br.com.drg.servicos.implementacoes;
+
+import br.com.drg.servicos.interfaces.UsuarioService;
+
+public class UsuarioServiceImplementacao implements UsuarioService {
+
+}
---
+++ src/main/java/br/com/drg/servicos/interfaces/UsuarioService.java
@@ -0,0 +1,5 @@
+package br.com.drg.servicos.interfaces;
+
+public interface UsuarioService {
+
+}
