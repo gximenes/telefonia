6a85866c12092aa6af6e7d35279963b6b606bfbf
giovani.b.ximenes@gmail.com <giovani.b.ximenes@gmail.com>
2017-06-08 20:44:16 -0300

    Commit

---
+++ src/main/java/br/com/dgr/enuns/Situacao.java
@@ -0,0 +1,25 @@
+package br.com.dgr.enuns;
+
+public enum Situacao {
+	
+	ATIVO("A", "Ativo"),
+	INATIVO("I", "Inativo");
+	
+	
+	private String valor;
+	private String descricao;
+
+	private Situacao(String vl, String desc) {
+		this.valor = vl;
+		this.descricao = desc;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+}
