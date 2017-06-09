94f28c0ddf2aac635ecd2cd59348e05343c530bb
giovani.b.ximenes@gmail.com <giovani.b.ximenes@gmail.com>
2017-06-08 22:10:38 -0300

    Commit

---
+++ src/main/java/br/com/dgr/utilitario/ProdutorDeLista.java
@@ -0,0 +1,35 @@
+package br.com.dgr.utilitario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.model.SelectItem;
+
+import br.com.dgr.enuns.Situacao;
+import br.com.dgr.enuns.TipoPessoa;
+
+@ApplicationScoped
+@ManagedBean
+public class ProdutorDeLista {
+
+
+	public List<SelectItem> getListaSituacaoSelectItem(){
+		List<SelectItem> itens = new ArrayList<SelectItem>();
+		for(Situacao st :Situacao.values()){
+			itens.add(new SelectItem(st.getValor(),st.getDescricao()));
+		}
+		return itens;
+	}
+
+	public List<SelectItem> getListaTipoPessoaSelectItem(){
+		List<SelectItem> itens = new ArrayList<SelectItem>();
+		for(TipoPessoa st :TipoPessoa.values()){
+			itens.add(new SelectItem(st.getValor(),st.getDescricao()));
+		}
+		return itens;
+	}
+
+
+}
