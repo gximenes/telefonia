15ad59b7988dd887d41dd54f14020d5693e82615
Douglas <douglasnc5@gmail.com>
2017-06-13 22:55:03 -0300

    Criação da dependência da biblioteca de conversão de áudio.

--- src/main/java/br/com/dgr/utilitario/ConversorAudio.java
+++ src/main/java/br/com/dgr/utilitario/ConversorAudio.java
@@ -4,9 +4,18 @@ import java.io.File;
 
 import it.sauronsoftware.jave.AudioAttributes;
 import it.sauronsoftware.jave.Encoder;
+import it.sauronsoftware.jave.EncoderException;
 import it.sauronsoftware.jave.EncodingAttributes;
+import it.sauronsoftware.jave.InputFormatException;
 
 public class ConversorAudio {
+	
+	/** Para esta classe funcionar, deve ser criada uma nova dependência
+	 * no arquivo pom.xml
+	 * mvn install:install-file -Dfile=C:\jave-1.0.2.jar 
+	 * -DgroupId=org.br.conversor -DartifactId=conversor 
+	 * -Dversion=1.0.2 -Dpackaging=jar -DgeneratePom=true
+	 */
 	
 	public void converteAudio(String caminhoAudioOrigem, String caminhoAudioConvertido, 
 			String tipoAudioOrigem, String tipoAudioConvertido){
@@ -21,6 +30,14 @@ public class ConversorAudio {
 		attrs.setFormat(tipoAudioConvertido);
 		attrs.setAudioAttributes(audio);
 		Encoder encoder = new Encoder();
-		encoder.encode(source, target, attrs);
+		try {
+			encoder.encode(source, target, attrs);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InputFormatException e) {
+			e.printStackTrace();
+		} catch (EncoderException e) {
+			e.printStackTrace();
+		}
 	}
 }
